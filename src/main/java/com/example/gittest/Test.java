9c5995e075807471f057a425bea1f04dbb9c7367
blue <blue.liu@waymap.cn>
2019-11-20 17:08:03 +0800

    提交一个文件

---
+++ src/main/java/com/example/gittest/Test.java
@@ -0,0 +1,12 @@
+package com.example.gittest;
+
+/**
+ * @author blue
+ * @description TODO
+ * @email blue.liu@waymap.cn
+ * @date 2019/11/20
+ */
+public class Test {
+    public static void main(String[] args) {
+    }
+}
